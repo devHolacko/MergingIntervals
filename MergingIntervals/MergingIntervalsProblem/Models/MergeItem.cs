d479075537302df0570dc90a09f7c8b553246cf1
Abdullah ElMenawy <Abdullah.ElMenawy@tpay.me>
2021-09-29 21:58:17 +0200

    initial commit

---
+++ MergingIntervals/MergingIntervalsProblem/Models/MergeItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergingIntervalsProblem.Models
+{
+    public class MergeItem
+    {
+        public TimeSpan ArrivalTime { get; set; }
+        public int Start { get; set; }
+        public int End { get; set; }
+        public string Action { get; set; }
+    }
+}
